26ad72a4988efbf47c875d5f21458047ed52a443
M <gregor.alic@gimb.org>
2021-02-15 22:50:33 +0100

    Add support in script_maker.py for older python versions

--- script_maker.py
+++ script_maker.py
@@ -7,12 +7,12 @@ x = y = 0
 z = 200
 pitch = yaw = roll = 0
 pitch = -20
-for i in range(0, 360):
+for i in range(0, 800):
     x = -450*math.cos(float(i)*3.14 / 180) + 450
     y = -450*math.sin(float(i)*3.15 / 180)
 
     yaw = i
-    f.write(f"camera-direction {yaw} {pitch} {roll}\n")
-    f.write(f"camera-move {x} {y} {z}\n")
+    f.write("camera-direction %s. %s. %s.\n" % (yaw, pitch, roll))
+    f.write("camera-move %s. %s. %s.\n" % (x, y, z))
     f.write("render\n")
 f.close()
