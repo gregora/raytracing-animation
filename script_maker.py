4bf5597d972867a7b279d4684689a9a20bd21192
Gregor A <gregor.alic@gimb.org>
2021-03-07 22:48:46 +0100

    Add another script

--- script_maker.py
+++ script_maker.py
@@ -1,18 +1,18 @@
 import math
 
 
-f = open("script2.txt", "w")
+f = open("script3.txt", "w")
 x = y = 0
-z = 200
+z = 2
 pitch = yaw = roll = 0
-pitch = -20
-for i in range(0, 800):
-    x = -450*math.cos(float(i)*3.14 / 180) + 450
-    y = -450*math.sin(float(i)*3.15 / 180)
+pitch = 0
+for i in range(0, 360):
+    x = -40*math.cos(float(i)*3.14 / 180) + 40
+    y = -40*math.sin(float(i)*3.15 / 180)
 
     yaw = i
     f.write("camera-direction %s. %s. %s.\n" % (yaw, pitch, roll))
     f.write("camera-move %s. %s. %s.\n" % (x, y, z))
-    f.write("sky-color 0.2 0.8 1\n")
+    f.write("sky-color 0.05 0.2 0.3\n")
     f.write("render\n")
 f.close()
